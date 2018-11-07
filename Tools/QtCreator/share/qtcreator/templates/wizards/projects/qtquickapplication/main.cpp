280479d41edac331d2e1efdd8da86434a0f1af4b
CsloudX <35988767+CsloudX@users.noreply.github.com>
2018-11-07 21:45:30 +0800

    V1

---
+++ Tools/QtCreator/share/qtcreator/templates/wizards/projects/qtquickapplication/main.cpp
@@ -0,0 +1,26 @@
+%{Cpp:LicenseTemplate}\
+%{JS: QtSupport.qtIncludes([], ["QtGui/QGuiApplication", "QtQml/QQmlApplicationEngine"])}
+int main(int argc, char *argv[])
+{
+@if %{SetQPAPhysicalSize}
+    qputenv("QT_QPA_EGLFS_PHYSICAL_WIDTH", QByteArray("213"));
+    qputenv("QT_QPA_EGLFS_PHYSICAL_HEIGHT", QByteArray("120"));
+
+@endif
+@if %{UseVirtualKeyboard}
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+@endif
+#if defined(Q_OS_WIN)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
